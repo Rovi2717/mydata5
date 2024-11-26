39bd4c788cf75ebb43d9dffa6e08c3a047f87000
Rovi2717 <137065428+Rovi2717@users.noreply.github.com>
2024-11-26 13:20:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
-provider "aws" {}
-resource "aws_instance" "WebServer" {
-  ami           = "ami-012967cc5a8c9f891"
-  instance_type = "t2.micro"
+provider "aws" {
+  region     = "ap-south-1"
+}
+
+
+resource "aws_instance" "ourfirst" {
+  ami           = "ami-0447a12f28fddb066"
+  instance_type = "t2.small"
+#  tags = {
+#    Name = "sentinel-server"
+#  }
+
 }
